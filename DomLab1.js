b5576c163d2388a0c8fcd4c8b58e7b9d728c44e9
simo <simo.mordi@gmail.com>
2023-08-22 13:12:55 -0700

    new

--- DomLab1.js
+++ DomLab1.js
@@ -1,12 +1,33 @@
-const heading = document.getElementById("heading");
-heading.textContent = "some other text";
+// const headingOne = document.getElementById("heading");
+// headingOne.textContent = "some text";
 
-const hideButton = document.getElementById("hide");
-hideButton.addEventListener("click", () => {
-  heading.style.display = "none";
-});
 
-const unhideButton = document.getElementById("unhide");
-unhideButton.addEventListener("click", () => {
-  heading.style.display = "block";
-});
+// const hideButton = document.getElementById("hide");
+// const unhideButton = document.getElementById("unhide");
+
+
+// hideButton.addEventListener("click", () => {
+//   headingOne.style.display = "none";
+// });
+
+// unhideButton.addEventListener("click",  () => {
+//   headingOne.style.display = "block";
+// });
+
+const headingOne = document.getElementById("heading");
+
+const hideOrUnhide = (act) => {
+    headingOne.style.display = act;
+};
+
+const action1 = () => {
+    hideOrUnhide("none")
+};
+
+const action2 = () => {
+    hideOrUnhide("block")
+};
+
+
+document.getElementById("hide").addEventListener("mouseover", action1);
+document.getElementById("unhide").addEventListener("click", action2);
